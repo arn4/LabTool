7ccc66847396644b180c8dc95fb0a158bc917985
Luca Arnaboldi <luca@arnaboldi.lu>
2020-04-27 12:00:44 +0200

    Updated my email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name = "LabTools",
     author  =  "Luca Arnaboldi",
-    author_email = "luca.arnaboldi@arn4.it",
+    author_email = "luca@arnaboldi.lu",
     description = "Package with useful tool for Physics Laborayory Courses",
     url = "https://github.com/arn4/LabTools",
     packages = setuptools.find_packages(),
