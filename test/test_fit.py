0f0d1de593559ba21cad58745eafaf9d98149f6a
Luca Arnaboldi <luca@arnaboldi.lu>
2020-05-26 19:20:54 +0200

    Added circle_fit

--- test/test_fit.py
+++ test/test_fit.py
@@ -14,10 +14,10 @@ uncertainties. It is only checking if it works in every case I need.
 """
 def model(x, a, b):
     return numpy.sqrt(a * x + b)
-        
+
 def umodel(x, a, b):
     return unumpy.sqrt(a * x + b)
-        
+
 def dumodel(x, a, b):
     return a / (2 * unumpy.sqrt(a * x + b))
 
@@ -39,13 +39,13 @@ def test_iteraed_fit_with_chi2():
     assert((p[0]-p0[0]).n < 0.01)
     assert((p[1] - p0[1]).n < 0.01)
     print(chi2iterated(umodel, p, X, Y, df = dumodel))
-    
+
     # Not Coverge
     try:
         p = iterated_fit(umodel, X, Y, df = dumodel, max_iterations = 0)
     except Warning:
         pass
-        
+
     # Numeric derivative
     try:
         p = iterated_fit(umodel, X, Y)
@@ -55,8 +55,25 @@ def test_iteraed_fit_with_chi2():
         chi2iterated(umodel, p, X, Y)
     except NotImplementedError:
         pass
-    
-    
+
+
 def test_ucurve_fit():
     test_ucurve_fit_()
-    
+
+def test_circle_fit_paper():
+    """
+    This test was extracted from the paper: "Least-Squares Circle" Fit by Randy Bullock.
+
+    It test correctness of the fit.
+    """
+    x = numpy.array([0.000, 0.500, 1.000, 1.500, 2.000, 2.500, 3.000])
+    y = numpy.array([0.000, 0.250, 1.000, 2.250, 4.000, 6.250, 9.000])
+    xc, yc, r = circle_fit(x,y)
+    assert(xc + 11.839 < 0.001)
+    assert(yc - 8.4464 < 0.0001)
+    assert(r  - 14.686 < 0.001)
+
+    # This is to check if it works also with uncertainties values
+    X = unarray(x, [0.001]*len(x))
+    Y = unarray(y, [0.001]*len(y))
+    Xc, Yc, R = circle_fit(X,Y)
