9300ed22b76b64aa38c16a8cc61082631b1b8891
Luca Arnaboldi <luca@arnaboldi.lu>
2020-08-03 18:09:20 +0200

    Prepared to package distribution

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@
 import setuptools
 
 setuptools.setup(
-    name = "LabTools",
-    author  =  "Luca Arnaboldi",
-    author_email = "luca@arnaboldi.lu",
-    description = "Package with useful tool for Physics Laborayory Courses",
-    url = "https://github.com/arn4/LabTools",
+    name = 'LabTools',
+    author  =  'Luca Arnaboldi',
+    author_email = 'luca@arnaboldi.lu',
+    description = 'Package with useful tool for Physics Laborayory Courses',
+    url = 'https://github.com/arn4/LabTools',
     packages = setuptools.find_packages(),
     python_requires = '>=3.6',
     install_requires = [
         'numpy',
         'uncertainties',
         'matplotlib',
-        'tikzplotlib',
         'scipy',
         'pyaml'
     ]
