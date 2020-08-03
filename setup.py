3f91abd52bb34824adb1008b14cb83f460f15e32
Luca Arnaboldi <luca@arnaboldi.lu>
2020-08-03 19:46:11 +0200

    Package support

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 
 import setuptools
+import pathlib
+
+from LabTools  import (__pkgname__ as PKG_NAME, __version__ as VERSION,
+                       __author__ as AUTHOR, __license__ as LICENSE,
+                       __summary__ as SUMMARY, __url__ as URL)
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
-    name = 'LabTools',
-    author  =  'Luca Arnaboldi',
+    name = PKG_NAME,
+    version = VERSION,
+    author  =  AUTHOR,
     author_email = 'luca@arnaboldi.lu',
-    description = 'Package with useful tool for Physics Laborayory Courses',
-    url = 'https://github.com/arn4/LabTools',
+    description = SUMMARY,
+    long_description = README,
+    long_description_content_type = 'text/markdown',
+    url = URL,
+    license = LICENSE,
     packages = setuptools.find_packages(),
     python_requires = '>=3.6',
     install_requires = [
